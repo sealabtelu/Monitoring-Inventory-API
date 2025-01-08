101343e60d495f17d6a9e06cdefb12b6af353e9c
szyxxx <axeldavid1521@gmail.com>
2025-01-08 15:55:34 +0700

    change json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const { google } = require('googleapis');
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./larabase.json');
+const serviceAccount = require('./larabase-2267e-firebase-adminsdk-i2i5o-2808f63f1f.json');
 
 const port = process.env.PORT || 8080;
 
@@ -24,7 +24,7 @@ const upload = multer();
 // Google Sheets API setup
 const sheets = google.sheets('v4');
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./rfidtelyu.json", // Path to your service account JSON file
+  keyFile: "./rfidtelyu-1833bc1f2ba7.json", // Path to your service account JSON file
   scopes: ['https://www.googleapis.com/auth/spreadsheets'], // Scopes for Google Sheets
 });
 
